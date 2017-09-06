b61f2427312d7fae1830af0f968a92a2d5a41956
Siladittya Manna <sadimanna@users.noreply.github.com>
2017-09-06 13:45:43 +0530

    Add files via upload

---
+++ kmeansppclustering.py
@@ -0,0 +1,86 @@
+import sys
+import numpy as np
+import scipy.io as sio
+#import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.svm import SVC
+
+filename = sys.argv[1]
+datafile = sio.loadmat(filename)
+data = datafile['bow']
+sizedata=[len(data), len(data[0])]
+disp = []
+optimal_ks = []
+#Determining the optimal number of k with gap statistic method
+def gap_statistic(data):
+	sizedata = [len(data),len(data[0])]
+	SD = []
+	gap = []
+	for knum in xrange(10,22):
+		print knum
+		#Clustering original Data
+		kmeanspp = KMeans(n_clusters=knum,init = 'k-means++',max_iter = 100,n_jobs = 1)
+		kmeanspp.fit(data)
+		dispersion = kmeanspp.inertia_
+		#Clustering Reference Data
+		nrefs = 10
+		refDisp = np.zeros(nrefs)
+		for nref in xrange(nrefs):
+			refdata = np.random.random_sample(tuple(sizedata))
+			refkmeans = KMeans(n_clusters=knum,init='k-means++',max_iter=100,n_jobs=1)
+			refkmeans.fit(refdata)
+			refdisp = refkmeans.inertia_
+			refDisp[nref]=np.log(refdisp)
+		mean_log_refdisp = np.mean(refDisp)
+		gap.append(mean_log_refdisp-np.log(dispersion))
+		sd = (sum([(r-m)**2 for r,m in zip(refDisp,[mean_log_refdisp]*nrefs)])/nrefs)**0.5
+		SD.append(sd)
+	SD = [sd*((1+(1/nrefs))**0.5) for sd in SD]
+	opt_k = None
+	diff = []
+	for i in xrange(len(gap)-1):
+		diff = (SD[i+1]-(gap[i+1]-gap[i]))
+		if diff>0:
+			opt_k = i+10
+			break
+	if opt_k < 22:
+		print opt_k
+		return opt_k
+	else:
+		return 22
+
+
+ntrials = 50
+for ntrial in xrange(ntrials):
+	print 'ntrial: ',ntrial
+	optimal_ks.append(gap_statistic(data))
+#plt.plot(np.linspace(10,19,10,True),gap)
+#plt.show()
+unique_opt_k = list(set(optimal_ks))
+k_count = {}
+count_opt_k = 0
+second_opt_k = 0
+opt_k = 0
+for u_o_k in unique_opt_k:
+	count = optimal_ks.count(u_o_k)
+	k_count[u_o_k]=count
+	if count>count_opt_k:
+		count_opt_k = count
+		opt_k = u_o_k
+	elif count==count_opt_k:
+		second_opt_k = u_o_k
+print opt_k
+print k_count
+##14, 0
+#Clusterin with optimal number of k
+#opt_k =14
+kmeanspp = KMeans(n_clusters = opt_k,init='k-means++',max_iter=100,n_jobs=1)
+kmeanspp.fit(data)
+centers = kmeanspp.cluster_centers_
+clusterlabels = kmeanspp.labels_
+print clusterlabels
+mdict = {}
+mdict['clusterlabels'] = clusterlabels
+
+sio.savemat('clusterlabels.mat',mdict,format = '4',oned_as = 'column')
+print 'dan dana dan done...'
