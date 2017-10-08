ce3bf59877ae5b9e63e54013716086769b31a03b
Siladittya Manna <sadimanna@users.noreply.github.com>
2017-10-08 13:21:39 +0530

    Add files via upload

--- isotropicmeasure.m
+++ isotropicmeasure.m
@@ -4,7 +4,8 @@ function isotropic_measure = isotropicmeasure(imgxx,imgyy,imgxy,sigmaD)
     for j=1:5
       Mu = (sigmaD^2)*[imgxx(i,j),imgxy(i,j);imgxy(i,j),imgyy(i,j)];
       eigvals = eig(Mu);
-      isotropic_measure = min(eigvals)/max(eigvals);
+      R(i,j) = min(eigvals)/max(eigvals);
     end
   end
-endfunction
+  isotropic_measure = R(3,3);
+end
--- optdiffscale.m
+++ optdiffscale.m
@@ -1,5 +1,5 @@
-function opts = optdiffscale(sigmaI,W0)
-  R = [];
+function opts = optdiffscale(sigmaI,W)
+  maxisotropic_measure = 0;
   %%Calculation of optimal s for calculating sigmaD by maximizing isotropic measure
   for s = 50:75
     sofD = s/100;
@@ -7,17 +7,17 @@ function opts = optdiffscale(sigmaI,W0)
     Lwy = [-1,0,1]'*fspecial('gaussian',[1 3],sigmaD);
     Lwx = fspecial('gaussian',[3 1],sigmaD)*[-1,0,1];
     gw = fspecial('gaussian',[3 3],sigmaI);
-    imgwx = conv2(W0,Lwx,'same');
-    imgwy = conv2(W0,Lwy,'same');
+    imgwx = conv2(W,Lwx,'same');
+    imgwy = conv2(W,Lwy,'same');
     imgwxx = imgwx.^2;
     imgwxx = conv2(imgwxx,gw,'same');
     imgwyy = imgwy.^2;
     imgwyy = conv2(imgwyy,gw,'same');
     imgwxy = imgwx.*imgwy;
     imgwxy = conv2(imgwxy,gw,'same');
-    R(s-49) = isotropicmeasure(imgwxx,imgwyy,imgwxy,sigmaD);
-  endfor
-  opts = (R(find(R==max(R)))+49)/100;
-  %disp('opts')
-  %disp(opts)
-endfunction
+    isotropic_measure = isotropicmeasure(imgwxx,imgwyy,imgwxy,sigmaD);
+    if isotropic_measure>maxisotropic_measure
+        maxisotropic_measure = isotropic_measure;
+        opts = sofD;
+    end
+end
