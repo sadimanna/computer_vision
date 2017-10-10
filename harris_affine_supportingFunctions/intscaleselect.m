4e4d1a0cccce936046990f2d9d54da538827bbd4
Siladittya Manna <sadimanna@users.noreply.github.com>
2017-10-10 10:38:59 +0530

    Add files via upload

--- harris_affine_supportingFunctions/intscaleselect.m
+++ harris_affine_supportingFunctions/intscaleselect.m
@@ -1,22 +1,28 @@
 function scale = intscaleselect(W)
+  sig0 = 1.0;
   ki = 1.2;
   scale = [];
   Wlog = [];
   for k=1:10
-    sigma = ki^(k-1);
-    Wlog(:,:,k) = double(abs(conv2(conv2(W,fspecial('gaussian',[3 3],sigma),'same'),fspecial('log',[3 3],sigma),'same')));
-  endfor
+    sigma = sig0*ki^(k-1);
+    Wlog(:,:,k) = (sigma^2)*imfilter(conv2(W,fspecial('gaussian',5,sigma),'same'),fspecial('log',5,sigma),'replicate','same','conv');
+  end
   logvalvec = [];
   for k=1:10
     logval = Wlog(3,3,k);
     logvalvec = [logvalvec,logval];
-  endfor
+  end
   for n=2:9
-    if logvalvec(n)==max(logvalvec) && logvalvec(n)~=0
+    if logvalvec(n)>logvalvec(n-1) && logvalvec(n)>logvalvec(n+1)
       scale = ki^(n-1);
-    endif
-  endfor
-  if length(scale)==0
+    end
+    if scale ~= max(logvalvec)
+        continue
+    else
+        break
+    end
+  end
+  if isempty(scale)
     scale = 1;
-  endif
-endfunction
+  end
+end
